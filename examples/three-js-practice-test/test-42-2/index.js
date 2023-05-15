354797357cc5df52b9162b43bfb6b9404f5acec1
towavephone <towavephone@outlook.com>
2023-05-15 11:02:33 +0000

    Deploying to gh-pages from  @ 819850fd7538b7628f270fd1ace8fb44bb319dfd 🚀

---
+++ examples/three-js-practice-test/test-42-2/index.js
@@ -0,0 +1,102 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
+  const fov = 75;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 25;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0.5, 1, 0.5);
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 0, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('white');
+
+  function addLight(x, y, z) {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(x, y, z);
+    scene.add(light);
+  }
+  addLight(-1, 2, 4);
+  addLight(1, -1, -2);
+
+  const planeWidth = 0.5;
+  const planeHeight = 1;
+  const geometry = new THREE.PlaneGeometry(planeWidth, planeHeight);
+
+  const loader = new THREE.TextureLoader();
+
+  function makeInstance(geometry, color, rotY, url) {
+    const base = new THREE.Object3D();
+    scene.add(base);
+    base.rotation.y = rotY;
+
+    [-1, 1].forEach((x) => {
+      const texture = loader.load(url, render);
+      texture.offset.x = x < 0 ? 0 : 0.5;
+      texture.repeat.x = 0.5;
+      const material = new THREE.MeshPhongMaterial({
+        color,
+        map: texture,
+        opacity: 0.5,
+        transparent: true,
+        side: THREE.DoubleSide
+      });
+
+      const mesh = new THREE.Mesh(geometry, material);
+      base.add(mesh);
+
+      mesh.position.x = x * 0.25;
+    });
+  }
+
+  makeInstance(geometry, 'pink', 0, '/GATSBY_PUBLIC_DIR/images/happyface.png'); /* threejs.org: url */
+  makeInstance(geometry, 'lightblue', Math.PI * 0.5, '/GATSBY_PUBLIC_DIR/images/hmmmface.png'); /* threejs.org: url */
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  let renderRequested = false;
+
+  function render() {
+    renderRequested = undefined;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+  }
+  render();
+
+  function requestRenderIfNotRequested() {
+    if (!renderRequested) {
+      renderRequested = true;
+      requestAnimationFrame(render);
+    }
+  }
+
+  controls.addEventListener('change', requestRenderIfNotRequested);
+  window.addEventListener('resize', requestRenderIfNotRequested);
+}
+
+main();
