8674368d37dc2b5d76b595744d0091c444988aad
towavephone <towavephone@outlook.com>
2023-10-17 04:00:49 +0000

    Deploying to gh-pages from  @ d3cb403889fc4234b6fefba74adac6440c91886c 🚀

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-75c366fa95455a888f10.js","66cf166452b8f20158edd570c7d08811"],["/app-d422a928e87e0d5c9801.js","ae9d36364b2c8504081866ae6983073c"],["/commons-29b119645bc5a9977ba5.js","0cc3796827d921ee4e1ec760f242512c"],["/index.html","0d75e03df793fb79d5c80381f2ba0e56"],["/manifest.webmanifest","bbbf16bc344dfe99f1b13de04ad23629"],["/offline-plugin-app-shell-fallback/index.html","27bd063b11b700e0f91a8cd59ea12ac8"]];
+var precacheConfig = [["/app-75c366fa95455a888f10.js","66cf166452b8f20158edd570c7d08811"],["/app-d422a928e87e0d5c9801.js","ae9d36364b2c8504081866ae6983073c"],["/commons-29b119645bc5a9977ba5.js","0cc3796827d921ee4e1ec760f242512c"],["/index.html","f606654e1e391502a4ece0125c320d27"],["/manifest.webmanifest","bbbf16bc344dfe99f1b13de04ad23629"],["/offline-plugin-app-shell-fallback/index.html","ad9184308e3a5c8e2251d9ab21b8d4f2"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
